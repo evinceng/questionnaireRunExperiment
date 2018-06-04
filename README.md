5339bca9547b1446797ce0c0d3d5ca4493e08838
evinceng <aslan.evin@gmail.com>
2018-06-04 11:40:24 +0200

    questionnaire run experiment is done

--- README.md
+++ README.md
@@ -2,65 +2,47 @@
 
 Preperation:
 
-1. Connect tobii eyetracker sensor
-2. Run tobii eyetracker dotnet app.
-3. Open the Mvc.py, go to  Run-> Configuration per file 
-4. In the "General Settings" section check "Command line options" if not checked and enter the username (for ex: evina)
-5. Press OK button 
-6. Have following fields in config:
+6. Have following fields in config in exposure meter:
     [MONGODB]
     Host = localhost
     Port = 27017
     DBName = mediaExposureTry
     
     [TOBII]
-    DBCollectionName = eyesRolling
-
-7. Open the circle in the experiment folder
+    DBCollectionName = finalAnswersQuest
 
 Run:
 1. Run the Mvc.py file
+2. input a userName(ex:evina)
 3. Press the start button through exposuremeter
-4. Press Connect button via tobii eytracker dotnet app
-5. Start rolling eyes on the circle
-6. When it is done press Disconnect button via tobii eyetracker dotnet app
-7. Close the exposuremeter app. to change the userName
-8. Change the userName as described in Preperation steps 3&4, and repeat the process for second user from step 1
-9. Press the stop button in exposuremeter and close the window
+4. Fill the prequestionnaire
+5. Close the prequestionnaire
+6. Press the stop button through exposuremeter
+7. Fill the postquestionnaire
+8. Close the postquestionnaire
+9. Close the exposuremeter app. 
 10.Run runExperiment.py in experiment folder having following configuration in the file with proper userNames and session info
 
 host = "localhost"
 port = 27017
 dbName = "mediaExposureTry"
-collectionName = "eyesRolling"
+collectionName = "finalAnswersQuest"
+
 
 # evina Configuration
-userName1 = "evina"
-Ts = 0.5 # Sampling frequency
-fCode = 1
-k = 3 
-tMin = 1.6
-tMax = 5
-
-# user2 Configuration
-userName2 = "user2"
-TsUser = 0.5 # Sampling frequency
-fCodeUser = 1
-kUser = 3 
-tMinUser = 3 # 1.49
-tMaxUser = 9 # 10.2
+userName1 = "evina" #the username that you inputted in step 2 above
 
 # fieldNames variables
-xCoordVariable = "leftGaze:x"
-yCoordVariable = "leftGaze:y"
+xCoordVariable = "relativeTime"
+yCoordVariable = "answer"
 timeStampVariable = "relativeTime"
 colorUponVariable = "relativeTime"
 userNameVariable = "userName"
 
 # query variables
-userNames = [ userName1, userName2] # "evina", "user2",
-sessions = [ ] #  "evina2018-05-10 14:33:21.185000", "user22018-05-14 12:06:23.842000",
-projectionFields = [ "userName", "relativeTime", "leftGaze:x", "leftGaze:y" ]
+userNames = [ userName1 ] # "evina", "user2",
+sessions = ["evina2018-06-01 16:02:59.355000" ]
+projectionFields = [ "userName", "relativeTime", "answer" ]
 relativeTime = [ ] # start, end # it will get all times if not provided
 
-11. See both produced log file and a graph having circles on the screen
+11. See both produced log file and a graph having numeric answers with relative times
--- runExperiment.py
+++ runExperiment.py
@@ -4,140 +4,37 @@ Created on Wed May 16 10:49:25 2018
 
 @author: evin
 """
+import sys
 
-def traceToFile(traceFileName, inputString):
-    """
-    Writes inputString to the file named traceFileName if trace is Enabled,
-    prints the inputString to the shell otherwise    
-    """
-    if traceEnabled:
-        traceFileName.write("{}".format(inputString))
-    else:
-        print "{}".format(inputString)
-        
-def traceToFileEOL(traceFileName, inputString):
-    """
-    Writes inputString followed by a new line to the file named traceFileName
-    if trace is enabled, prints the inputString to the shell otherwise    
-    """
-    if traceEnabled:
-        traceFileName.write("{}".format(inputString) + "\n")
-    else:
-        print "{}".format(inputString) + "\n"
+utilsPath = "../../../03-Tools/00-Utils/"
 
-def traceToFileLists(traceFileName, itemList, listName, itemName):
-    """
-    Writes the elements of itemList with the itemListName followed by each
-    element with the itemName tag, provided that it is not empty
-    """
-    if itemList:
-        traceToFileEOL(traceFileName, listName)
-        for item in itemList:
-            traceToFileEOL(traceFileName, itemName + str(item))
-        traceToFileEOL(traceFileName, "")
-        
-def tracetoUsersExpDesVariables(userName, tMin, tMax, k, Ts, fCode):
-    """
-    Writes given parameters associated with userName to the file
-    """
-    traceToFileEOL(traceFileName, userName + " tMin: " + str(tMin)) 
-    traceToFileEOL(traceFileName, userName + " tMax: " + str(tMax))
-    traceToFileEOL(traceFileName, userName + " k: " + str(k))
-    traceToFileEOL(traceFileName, userName + " Ts: " + str(Ts))
-    traceToFileEOL(traceFileName, userName + " fCode: " + str(fCode))
-    traceToFileEOL(traceFileName, "")
-    
-def constructProjectionFileds(getID = False):
-    """
-    Constructs queryable projected fields dictionary for inputting find statement's project for mongodb 
-    """
-    fields = { }
-    if not getID:
-        fields["_id"] = 0
-        
-    for field in projectionFields:
-        fields[field] = 1
-        
-    return fields
+if utilsPath not in sys.path:
+    sys.path.append(utilsPath)
 
-def constructQuery():
-    """
-    Constructs mongodb query dictionary for inputting find statement's query for mongodb 
-    """
-    query = { }
-    
-    if userNames:
-        query["userName"] = { "$in": userNames }
-        
-    if sessions:
-        query["sessionID"] = { "$in": sessions }
-        
-    if relativeTime:
-        query["relativeTime"] = { "$gt": relativeTime[0], "$lt": relativeTime[1] }
-        
-    #print query
-    
-    return query
-
-def interpolateAndPlot(xIn, yIn, tIn, tMin, tMax, k, Ts, fCode):
-    """
-    interpolates given xIn, yIn acoording to times tMin, tMax and other inputs,
-    and plots them on a graph
-    """
-#    tIn = timeStamps#np.array(timeStamps)
-#    xIn = coordX#np.array(coordX)
-#    yIn = coordY#np.array(coordY)
-    interpolatedXOnTime = interpolateBS(tIn, xIn, tMin, tMax, k, Ts, fCode)
-    interpolatedYOnTime = interpolateBS(tIn, yIn, tMin, tMax, k, Ts, fCode)
-    
-    sampledTime = np.linspace(tMin, tMax, 1000)
-    interpolatedXOnSampledTime = splev(sampledTime, interpolatedXOnTime)
-    interpolatedYOnSampledTime = splev(sampledTime, interpolatedYOnTime)
-    
-    fig = plt.figure()
-    ax = fig.gca() 
-    ax.scatter(xIn, yIn, c=tIn, cmap=cm.seismic)
-    ax.plot(interpolatedXOnSampledTime, interpolatedYOnSampledTime, color="green")
-    ax.legend()
-    plt.show()    
-
-def getAbsPath(fileName):
-    """
-    returns absolute path of a given fileName
-    """
-    APP_FOLDER = os.getcwd()#path.dirname(os.path.realpath(sys.argv[0]))
-    filePath = os.path.join(APP_FOLDER, fileName)
-    return filePath
 # ===========================================================================================================        
 ## 1. Short description -------------------------------------------------------------------------------------
-# @brief An analysis to see two circles on a graph via interpolating two users rolling eyes eyeGaze information through time  
+# @brief An analysis to see the answers to the pre and post questionnaire
 # @author EvinA
-# @time 2018-05-16
+# @time 2018-06-04
 
 
 # ===========================================================================================================
 ## 2. Configuration, libraries -----------------------------------------------------------------------------
-# Data
-#abs_path = "C:\Users\evin\Documents\GitHub\experiment"
-abs_path = "C:\Users\evin\Documents\GitHub\experiment"
+
 # Trace files 
-rel_path = ""
-coreTraceFileName = "_RollingEyes_trace.txt"
+coreTraceFileName = "_Questionnaire_trace.txt"
 traceEnabled = True
 
 # Load libraries -------------------------------------------------------------------------------------------
 from pymongo import MongoClient
 from pymongo.errors import ConnectionFailure
 import sys
-import os
 import platform
 import matplotlib.pyplot as plt
-from matplotlib import cm
-import numpy as np
-from scipy.interpolate import splev
-from interpolate_funs import interpolateBS 
 from datetime import datetime
-
+from numbers import Number
+import FileUtils
+import QueryUtils
 # ===========================================================================================================
 # 3. Experimental design ---------------------------------------------------------------------------------------
 
@@ -145,35 +42,25 @@ from datetime import datetime
 host = "localhost"
 port = 27017
 dbName = "mediaExposureTry"
-collectionName = "eyesRolling"
+collectionName = "finalAnswersQuest"
 
 # evina Configuration
 userName1 = "evina"
-Ts = 0.5 # Sampling frequency
-fCode = 1
-k = 3 
-tMin = 1.6 #0.5
-tMax = 5   # 9.5 
 
 # user2 Configuration
-userName2 = "user2"
-TsUser = 0.5 # Sampling frequency
-fCodeUser = 1
-kUser = 3 
-tMinUser = 1 # 1.49 #0.018
-tMaxUser = 6 # 10.2 # 6.8
+#userName2 = "user2"
 
 # fieldNames variables
-xCoordVariable = "leftGaze:x"
-yCoordVariable = "leftGaze:y"
+xCoordVariable = "relativeTime"
+yCoordVariable = "answer"
 timeStampVariable = "relativeTime"
 colorUponVariable = "relativeTime"
 userNameVariable = "userName"
 
 # query variables
-userNames = [ userName1, userName2] # "evina", "user2",
-sessions = ["user22018-05-18 13:56:11.521000", "evina2018-05-18 13:50:15.285000" ] #  "evina2018-05-10 14:33:21.185000", "user22018-05-14 12:06:23.842000",   #2222 user22018-05-18 13:56:11.521000, evina2018-05-18 13:50:15.285000
-projectionFields = [ "userName", "relativeTime", "leftGaze:x", "leftGaze:y" ]
+userNames = [ userName1 ] # "evina", "user2",
+sessions = ["evina2018-06-01 16:02:59.355000" ]
+projectionFields = [ "userName", "relativeTime", "answer" ]
 relativeTime = [ ] # start, end # it will get all times if not provided
 
 # ============================================================================================================
@@ -190,86 +77,64 @@ except ConnectionFailure, e:
 dbh = client[dbName]
 collection = dbh[collectionName]
 
-projection = constructProjectionFileds()
-query = constructQuery()
+projection = QueryUtils.constructProjectionFileds(projectionFields)
+query = QueryUtils.constructQuery(userNames, sessions, relativeTime)
     
-resultList = list(collection.find( query, projection))
+resultList = list(collection.find(query, projection))
+print resultList
 
 # construct users dict-----------------------------------------------
-users = { }
-for userName in userNames:
-    users[userName] = { }
-    for field in projectionFields:
-        if field != userNameVariable:
-            users[userName][field] = [info[field] for info in resultList if info[userNameVariable] == userName]
-#=========================================================================
+users = QueryUtils.createUsersDict(userNameVariable, userNames, projectionFields, resultList)
             
-# identify x and y coordinatesand timestamps info of the users to give as input to interpolate and to use while plotting the graph
-xCoord = users[userName1][xCoordVariable]
-yCoord = users[userName1][yCoordVariable]
-timeStamps = users[userName1][timeStampVariable]
-
-xCoordUser = users[userName2][xCoordVariable]
-yCoordUser = users[userName2][yCoordVariable]
-timeStampsUser = users[userName2][timeStampVariable]
-    
 # Create trace file
 currentTime = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
-#traceFileName = abs_path + rel_path + currentTime + coreTraceFileName
-if traceEnabled:
-    path = getAbsPath(currentTime + coreTraceFileName)
-    traceFileName = open(path, "w")
+
+#if traceEnabled:
+path = FileUtils.getAbsPath(currentTime + coreTraceFileName)
+traceFileName = open(path, "w")
 
 
 # =============================================================================================================
 # 5. Report ---------------------------------------------------------------------------------------------------
 # Basics
-traceToFileEOL(traceFileName, "An analysis to see two circles on a graph via interpolating two users eyeGaze information taken from Tobii eyetracker through time, while rolling eyes")
-traceToFileEOL(traceFileName, "Time: " + currentTime)
-traceToFileEOL(traceFileName, "OS: " + platform.system())
-traceToFileEOL(traceFileName, "Python ver: " + platform.python_version())
+FileUtils.traceToFileEOL(traceEnabled, traceFileName, "An analysis to see two circles on a graph via interpolating two users eyeGaze information taken from Tobii eyetracker through time, while rolling eyes")
+FileUtils.traceToFileEOL(traceEnabled, traceFileName, "Time: " + currentTime)
+FileUtils.traceToFileEOL(traceEnabled, traceFileName, "OS: " + platform.system())
+FileUtils.traceToFileEOL(traceEnabled, traceFileName, "Python ver: " + platform.python_version())
 
 # Experimental design
-traceToFileEOL(traceFileName, "")
-traceToFileLists(traceFileName, userNames, "Extracted Users:", "User: ")
-traceToFileLists(traceFileName, sessions, "Extracted Sessions:", "Session: ")
-traceToFileLists(traceFileName, projectionFields, "Extracted Fileds:", "Field: ")
-traceToFileLists(traceFileName, relativeTime, "Extracted Time Period:", "relativeTime: ")
-
-tracetoUsersExpDesVariables(userName1, tMin, tMax, k, Ts, fCode)
-tracetoUsersExpDesVariables(userName2, tMinUser, tMaxUser, kUser, TsUser, fCodeUser)
+FileUtils.traceToFileEOL(traceEnabled, traceFileName, "")
+FileUtils.traceToFileLists(traceEnabled, traceFileName, userNames, "Extracted Users:", "User: ")
+FileUtils.traceToFileLists(traceEnabled, traceFileName, sessions, "Extracted Sessions:", "Session: ")
+FileUtils.traceToFileLists(traceEnabled, traceFileName, projectionFields, "Extracted Fileds:", "Field: ")
+FileUtils.traceToFileLists(traceEnabled, traceFileName, relativeTime, "Extracted Time Period:", "relativeTime: ")
 
 # ============================================================================================================
 # 6. Implement Experiment --------------------------------------------------------------------------------------------
-
-#interpolate xCoord and yCoord with the timeStamps and other inputs for first user
-interpolatedXOnTime = interpolateBS(timeStamps, xCoord, tMin, tMax, k, Ts, fCode)
-interpolatedYOnTime = interpolateBS(timeStamps, yCoord, tMin, tMax, k, Ts, fCode)
-
-#sample the time and evaluate interpolation on sampledTime for first user
-sampledTime = np.linspace(tMin, tMax, 1000)
-interpolatedXOnSampledTime = splev(sampledTime, interpolatedXOnTime)
-interpolatedYOnSampledTime = splev(sampledTime, interpolatedYOnTime)
-    
-#interpolate xCoord and yCoord with the timeStamps and other inputs for second user
-interpolatedXOnTimeUser = interpolateBS(timeStampsUser, xCoordUser, tMinUser, tMaxUser, kUser, TsUser, fCodeUser)
-interpolatedYOnTimeUser = interpolateBS(timeStampsUser, yCoordUser, tMinUser, tMaxUser, kUser, TsUser, fCodeUser)
-    
-#sample the time and evaluate interpolation on sampledTime for second user
-sampledTimeUser = np.linspace(tMinUser, tMaxUser, 1000)
-interpolatedXOnSampledTimeUser = splev(sampledTimeUser, interpolatedXOnTimeUser)
-interpolatedYOnSampledTimeUser = splev(sampledTimeUser, interpolatedYOnTimeUser)
-
-# Plot figures
-fig = plt.figure()
-ax = fig.gca() 
-ax.scatter(xCoord, yCoord, c=timeStamps, cmap=cm.seismic)
-ax.scatter(xCoordUser, yCoordUser, c=timeStampsUser, cmap=cm.PiYG)
-ax.plot(interpolatedXOnSampledTime, interpolatedYOnSampledTime, color="green")
-ax.plot(interpolatedXOnSampledTimeUser, interpolatedYOnSampledTimeUser, color="yellow")
-ax.legend()
+#=========================================================================
+#print users
+# identify x and y coordinatesand for plotting the graph
+xCoord = users[userName1][xCoordVariable]
+#print xCoord
+#print "@@@@@@@"
+yCoord = users[userName1][yCoordVariable]
+timeStamps = users[userName1][timeStampVariable]
+#print yCoord
+#print "@@@@@@@"
+xCoordNumeric = []
+yCoordNumeric = []
+for i in range(0, len(yCoord)):
+    if isinstance(yCoord[i], Number):
+        yCoordNumeric.append(yCoord[i])
+        xCoordNumeric.append(xCoord[i])
+   
+#Plot figures
+plt.scatter(xCoordNumeric, yCoordNumeric)
+plt.title(yCoordVariable + " versus " + xCoordVariable)
+plt.xlabel(xCoordVariable)
+plt.ylabel(yCoordVariable)
 plt.show()
-figPath = getAbsPath(currentTime + "_RollingEyes.pdf")
+figPath = FileUtils.getAbsPath(currentTime + "_Questionnaire.pdf")
 plt.savefig(figPath)
 
 ## Close trace files -------------------------------------------------------------------------------------------------
